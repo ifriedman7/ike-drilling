32871684f6b56b4d22a35bbe49971275db8df4bc
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2023-01-04 13:35:31 +0200

    rw

--- reader/reader.sh
+++ reader/reader.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #reader program: listen on port 3000--read msg and send msg to localhost port 37529
-coproc ncat -l localhost 3000
+coproc ncat -l -k localhost 3000
 
-while :
-do
-  read -r msg;
+while read -r msg; do
   echo $msg | nc localhost 37529
 done <&"${COPROC[0]}"
 
