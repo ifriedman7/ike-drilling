6f8a5aea85746276c97f85e1c9c6536860852569
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2023-01-04 18:29:48 +0200

    rw

--- reader/reader.sh
+++ reader/reader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #reader program: listen on port 3000--read msg and send msg to localhost port 37529
-coproc ncat -l -k localhost 3000
+coproc ncat -l -k reader 3000
 
 while read -r msg; do
   echo $msg | nc writer 37529
